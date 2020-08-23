add8962fda427cc36b90478caac14ba528775d6a
amit4257 <amitkumar11021999@gmail.com>
2020-08-23 17:31:58 +0530

    first commit

---
+++ devops_b1_lab2/src/main/java/devops_b1_lab2/Test.java
@@ -0,0 +1,14 @@
+package devops_b1_lab2;
+
+public class Test {
+ private int n=10;
+ public void f1(int n1)
+ {
+	 n1=20;
+ }
+	public static void main(String[] args) {
+		System.out.println("hello sonar");
+
+	}
+
+}
